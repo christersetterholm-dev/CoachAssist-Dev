de1c4fc1b69e1e0893ca262ec3804228f18af4de
christersetterholm-dev <christer.setterholm@gmail.com>
2026-07-27 23:21:00 +0200

    chore: bump application version to 1.2.5

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = '1.2.4';
-export const APP_BUILD_DATE = '2026-07-26';
+export const APP_VERSION = '1.2.5';
+export const APP_BUILD_DATE = '2026-07-27';
 export const APP_NAME = 'CoachAssist';
